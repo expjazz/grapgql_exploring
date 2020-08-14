c9e43640c0ea56dfae03bf9bb8405ae5f17ea152
Expedito Andrade <expeditojazz@gmail.com>
2020-08-14 16:22:35 -0300

    add login

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -36,4 +36,31 @@ const createCurriculum = async (parent, args, context) => {
   return newCurriculum;
 };
 
-module.exports = { signUp, createCurriculum };
+const login = async (parent, { email, password }, context) => {
+  const user = await context.prisma.user
+    .findOne({ where: { email } });
+  if (!user || email.length === 0) {
+    throw new Error(`Wrong information for ${email}`);
+  }
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) throw new Error('Invalid Password');
+
+  const token = jwt.sign({
+    userId: user.id,
+  }, process.env.APP_SECRET);
+
+  context.response.cookie('token', token, {
+    httpOnly: true,
+    maxAge: 100 * 60 * 60 * 24 * 365,
+  });
+  return user;
+};
+
+const signOut = async (_, __, context) => {
+  context.response.clearCookie('token');
+  return { message: 'Goodbye' };
+};
+
+module.exports = {
+  signUp, createCurriculum, login, signOut,
+};

567743541d46fe0b65212a813003a6fa3a11ae7a
Expedito Andrade <expeditojazz@gmail.com>
2020-08-15 09:43:19 -0300

    add current user query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,14 @@
 const users = (_, __, context) => context.prisma.user.findMany();
 
-const currentCurriculum = (_, args, context) => context.prisma.curriculum.findOne({ where: { id: args.curriculumId } });
+const currentUser = (parent, args, context) => {
+  if (!context.request.userId) {
+    return null;
+  }
+  return context.prisma.findOne({
+    where: { id: context.request.userId },
+  });
+};
+const currentCurriculum = (_, args, context) => context.prisma
+  .curriculum.findOne({ where: { id: args.curriculumId } });
 
-module.exports = { users, currentCurriculum };
+module.exports = { users, currentCurriculum, currentUser };
